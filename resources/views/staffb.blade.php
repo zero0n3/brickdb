24b00027b79637f8829714be2e490a355dea05f3
zero0n3 <ilsimonone@gmail.com>
2018-02-08 17:49:37 +0100

    staff blade

--- resources/views/staffb.blade.php
+++ resources/views/staffb.blade.php
@@ -12,16 +12,26 @@
 	@if($staff)
 	  <ul>
 		  @foreach($staff as $person)
-		    <li>{{$person['name']}}   {{$person['lastname']}} </li>
+		    <li>{{$loop->iteration}} {{$person['name']}}   {{$person['lastname']}} </li>
 		  @endforeach
 	  </ul>
+	 @else
+	 <p>No stuff</p>
 
 	@endif
+
+<h3>secondo metodo di ciclo</h3>
+	@forelse ($staff as $person)
+	  <li>{{$person['name']}}   {{$person['lastname']}} </li>
+	@empty
+	  <li>no staff</li>
+	@endforelse
+
 @endsection
 
 @section('footer')
 	@parent
 	<script>
-		alert('footer');
+		//alert('footer');
 	</script>
 	@stop
