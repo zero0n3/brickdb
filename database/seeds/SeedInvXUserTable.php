d29f67c83ead4b0b33373f9b1bae74f717c77d91
zero0n3 <ilsimonone@gmail.com>
2018-02-20 11:04:05 +0100

    fase seeding a posto credo

--- database/seeds/SeedInvXUserTable.php
+++ database/seeds/SeedInvXUserTable.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Models\Inventory_list;
 use App\Models\Inventory_x_user;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
+use League\Csv\Reader;
 
 class SeedInvXUserTable extends Seeder
 {
@@ -14,9 +17,24 @@ class SeedInvXUserTable extends Seeder
     {
         Inventory_x_user::truncate();
         
+        $csv = Reader::createFromPath(base_path().'/database/csv/inventory_x_user.csv')
+            ->setHeaderOffset(0);
 
+        $sql = 'INSERT INTO inventory_x_users (part_id, color_id, quantity, inventory_list_id, created_at) values (:part_id, :color_id, :quantity, :inventory_list_id, :created_at)';
 
-        
-        //factory(App\Models\Inventory_x_user::class, 6)->create();
+        //by setting the header offset we index all records
+        //with the header record and remove it from the iteration
+
+        foreach ($csv as $record) {
+            DB::statement($sql, [
+                'part_id' => $record['part_id'],
+                'color_id' => $record['color_id'],
+                'quantity' => $record['quantity'],
+                'inventory_list_id' => Inventory_list::first()->id,
+                'created_at' => Carbon::now(),
+
+            ]);
+
+        }
     }
 }
--- database/seeds/SeedMocXUserTable.php
+++ database/seeds/SeedMocXUserTable.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Models\Moc_list;
 use App\Models\Moc_x_user;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
+use League\Csv\Reader;
 
 class SeedMocXUserTable extends Seeder
 {
@@ -14,6 +17,24 @@ class SeedMocXUserTable extends Seeder
     {
         Moc_x_user::truncate();
         
-        //factory(App\Models\Moc_x_user::class, 6)->create();
+        $csv = Reader::createFromPath(base_path().'/database/csv/moc_x_user.csv')
+            ->setHeaderOffset(0);
+
+        $sql = 'INSERT INTO moc_x_users (part_id, color_id, quantity, moc_list_id, created_at) values (:part_id, :color_id, :quantity, :moc_list_id, :created_at)';
+
+        //by setting the header offset we index all records
+        //with the header record and remove it from the iteration
+
+        foreach ($csv as $record) {
+            DB::statement($sql, [
+                'part_id' => $record['part_id'],
+                'color_id' => $record['color_id'],
+                'quantity' => $record['quantity'],
+                'moc_list_id' => Moc_list::first()->id,
+                'created_at' => Carbon::now(),
+
+            ]);
+
+        }
     }
 }
