9a92d94e0271943abdd5233aac449a4e2da01da3
zero0n3 <ilsimonone@gmail.com>
2018-02-20 13:29:25 +0100

    location go on

--- database/migrations/2018_02_20_105612_create_locations_table.php
+++ database/migrations/2018_02_20_105612_create_locations_table.php
@@ -15,6 +15,12 @@ class CreateLocationsTable extends Migration
     {
         Schema::create('locations', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('part_id')->unsigned();
+            $table->foreign('part_id')->on('inventory_x_users')->references('id');
+            $table->string('first_loc',20);
+            $table->string('other_loc',20);
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->on('users')->references('id')->onDelete('cascade')->onUpdate('cascade');
             $table->timestamps();
         });
     }
