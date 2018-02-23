7727d01459137f20f68b1f8a0b338cf2ac6bec89
zero0n3 <ilsimonone@gmail.com>
2018-02-23 13:41:42 +0100

    cambio cartella

--- app/Http/Controllers/InventoryListsController.php
+++ app/Http/Controllers/InventoryListsController.php
@@ -14,7 +14,7 @@ class InventoryListsController extends Controller
 	public function index(){
 		//return Inventory_list::All();
 		$inventory_lists = Inventory_list::All();
-		return view('inventory', ['inventory_lists' => $inventory_lists]);
+		return view('inventory.inventory', ['inventory_lists' => $inventory_lists]);
 	}
 
 	public function delete( $id ){
