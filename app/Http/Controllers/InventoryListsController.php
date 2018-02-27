e85fbbe1bb0d784dc4cd2d3f8352f2d287a53d1e
zero0n3 <ilsimonone@gmail.com>
2018-02-27 14:26:55 +0100

    db facade

--- app/Http/Controllers/InventoryListsController.php
+++ app/Http/Controllers/InventoryListsController.php
@@ -11,28 +11,37 @@ use Illuminate\Support\Facades\DB;
 class InventoryListsController extends Controller
 {
     //
-	public function index(){
+	public function index(Request $req){
+		//$queryBuilder = DB::table('inventory_lists')->orderBy('id', 'desc');
+		$queryBuilder = Inventory_list::orderBy('id', 'desc');
+		if ($req->has('id')){
+
+			$queryBuilder->where('id','=',$req->input('id'));
+		}
+
+		$inventory_lists = $queryBuilder->get();
 		//return Inventory_list::All();
-		$inventory_lists = Inventory_list::All();
+		//$inventory_lists = Inventory_list::All();
 		return view('inventory.inventory', ['inventory_lists' => $inventory_lists]);
 	}
 
 	public function delete( $id ){
         
-        $sql = 'DELETE from inventory_lists WHERE id = :id';
+        $resu = Inventory_list::where('id', $id)->delete();
         
+        //$sql = 'DELETE from inventory_lists WHERE id = :id';
         //qui devo dare il return del db delete così da catturarlo in ajax 
         //e testarlo per eliminare la riga
-        return DB::delete($sql, ['id' => $id]);
-       
+        //return DB::delete($sql, ['id' => $id]);
+       	return $resu;
         //return redirect()->back();
 	}
 
 	
 	public function show($id){
 		
-		return Inventory_list::All()->where('id','=', $id);
-
+		$resu = Inventory_list::where('id','=', $id)->orderBy('id','desc')->get();
+		return $resu;
 		//return view('inventory', ['inventory_lists' => $inventory_lists]);
 	}
 
@@ -47,13 +56,19 @@ class InventoryListsController extends Controller
 	}
 
 	public function store($id, Request $req){
-		
 
+		$resu = Inventory_list::where('id', $id)->update(
+			[
+				'list_name' => request()->input('list_name'),
+			]
+		);
+
+/*
 		$data = request()->only('list_name');
 		$data['id'] = $id;
 		$sql = "UPDATE inventory_lists SET list_name = :list_name WHERE id = :id";
 		$res = DB::update($sql, $data);
-
+*/
 /*
 
 		$inventory_list = Inventory_list::find($id);
@@ -63,7 +78,7 @@ class InventoryListsController extends Controller
 		$res = $inventory_list->save();
 */
 		
-		$messaggio = $res ? 'Album con id ' .$id. ' aggiornato' : 'Album ' .$id. ' non aggiornato';
+		$messaggio = $resu ? 'inventory con id ' .$id. ' aggiornata' : 'Inventory ' .$id. ' non aggiornata';
 
 		session()->flash('message', $messaggio);
 		return redirect()->route('inventory');
@@ -78,7 +93,25 @@ class InventoryListsController extends Controller
 
 	public function save(Request $req){
 		
-		dd(request()->all());
+		$resu = Inventory_list::insert(
+			[
+				'list_name' => request()->input('list_name'),
+				'user_id' => 2,
+			]
+		);
 
+/*
+		$data = request()->only('list_name');
+		$data['user_id'] = 2;
+		
+      	$sql = 'INSERT INTO inventory_lists (list_name, user_id)';
+      	$sql .= ' VALUES (:list_name, :user_id)';
+      	
+      	$res = DB::insert($sql, $data);
+*/
+		$messaggio = $resu ? 'Inventory ' .request()->input('list_name'). ' inserita' : 'Inventory ' .request()->input('list_name'). ' non inserita';
+
+		session()->flash('message', $messaggio);
+		return redirect()->route('inventory');
 	}
 }
--- routes/web.php
+++ routes/web.php
@@ -13,17 +13,28 @@
 
 use App\Http\Controllers\InventoryListsController;
 use App\Models\Part;
+use Illuminate\Support\Facades\DB;
 
 Route::get('/','HomeController@index');
 
 //inventory lists
 Route::get('/inventory', 'InventoryListsController@index')->name('inventory');
 Route::get('/inventory/{inventory_list_id}/edit', 'InventoryListsController@edit');
-Route::delete('/inventory/{inventory_list_id}','InventoryListsController@delete');
-Route::get('/inventory/{inventory_list_id}', 'InventoryListsController@show');
-Route::patch('/inventory/{inventory_list_id}', 'InventoryListsController@store');
+Route::get('/inventory/{inventory_list_id}', 'InventoryListsController@show')->where('inventory_list_id', '[0-9]+');
 Route::get('/inventory/create', 'InventoryListsController@create')->name('create.inventory');
+Route::delete('/inventory/{inventory_list_id}','InventoryListsController@delete');
+Route::patch('/inventory/{inventory_list_id}', 'InventoryListsController@store');
 Route::post('/inventory', 'InventoryListsController@save')->name('inventory.save');
+
+Route::get('usernoinventory', function(){
+	$usernoinventory = DB::table('users as u')
+							->leftJoin('inventory_lists as i', 'u.id', 'i.user_id')
+							->select('u.id', 'email', 'name', 'i.list_name')
+							->whereNull('i.list_name')
+							->get();
+	return $usernoinventory;
+	
+});
 
 Route::get('welcome/{name?}/{lastname?}', 'WelcomeController@welcome')
 
